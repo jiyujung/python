a02ac016fe92a08589e72c4c8cb7b6c3a89969e6
jiyujung <s2019w39@e-mirim.hs.kr>
2020-12-27 19:15:20 +0900

    문자열 다루기

---
+++ II. 변수와 자료형/about_string.py
@@ -0,0 +1,10 @@
+greeting = 'Hello'
+to = "world!"
+print(greeting)     # str
+say_hello = greeting + ", " + to
+print(say_hello)    # Hello, world!
+print("Hello" * 5)  # HelloHelloHelloHelloHello
+print("\"Hello\"\n" + to)   # "Hello"\nworld!
+multiline = """Happy
+Programming"""
+print(multiline)    # Happy\nProgramming
