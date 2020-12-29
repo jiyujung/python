082cb68d567047acbce18c7cab3b4756971f855f
jiyujung <s2019w39@e-mirim.hs.kr>
2020-12-29 21:01:11 +0900

    인덱싱

---
+++ II. 변수와 자료형/string_indexing.py
@@ -0,0 +1,8 @@
+# 인덱싱 : 문자로 이루어진 문자열에서 원하는 자리의 문자를 가져온다.
+# 형식 : 문자열[인덱스]
+s = "Life is too short."
+print(s[3])     # e
+print(s[11])    #
+print(s[-1])    # .
+print(s[-7])    #
+print(s[-18])   # L
