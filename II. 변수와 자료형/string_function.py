5afb3a1869670db0a49301b4b11c08a6639067f1
jiyujung <s2019w39@e-mirim.hs.kr>
2021-01-01 22:22:13 +0900

    문자열 함수

--- II. 변수와 자료형/string_function.py
+++ II. 변수와 자료형/string_function.py
@@ -9,3 +9,9 @@ print(h.replace("Happy", "Funny"))  # 문자열 대체하기 /   Funny Programmi
 print(h.find("ap"))                 # h 문자열에서 인자 'ap'를 왼쪽부터 찾기 / 3
 print(h.rfind("a"))                 # h 문자열에서 인자 'ap'를 오른쪽부터 찾기 / 13
 print(h.find("zoo"))                # 찾지 못하면 -1 리턴
+print("a" in h)                     # h 문자열에 'a'가 있는지 확인하기 / True
+print("amp" in h)                   # h 문자열에 'amp'가 있는지 확인하기 / False
+x = "01::23::ab::&&"
+y = x.split("::")                   # x 문자열을 '::'로 나누어 리스트 만들기
+print(y)                            # ['01', '23', 'ab', '&&']
+print(", ".join(y))                 # y 리스트를 ', '로 이어서 문자열 만들기 / 01, 23, ab, &&
