10ab1b3e3aaf09842b3aef9fab7f44eff36b8a13
jiyujung <s2019w39@e-mirim.hs.kr>
2021-01-05 21:27:41 +0900

    리스트 생성

--- II. 변수와 자료형/list_about.py
+++ II. 변수와 자료형/list_about.py
@@ -2,9 +2,9 @@
 # 형식 : [값1, 값2, 값3, 값4]
 
 l = []      # 빈 리스트
-player = ["Messi", 10, True]    # ['Messi', 10, True]
-list("Happy")       #  ['H', 'a', 'p', 'p', 'y']
-list((1125, 2436))      # [1125, 2436]
-list({"menu":"pizza", "price":10000})       # ['menu', 'price']
-list({"사과", "배"})       # ['사과', '배']
-nums = list(range(3))       # [0, 1, 2]
+player = ["Messi", 10, True]            # ['Messi', 10, True]
+list("Happy")                           # ['H', 'a', 'p', 'p', 'y']
+list((1125, 2436))                      # [1125, 2436]
+list({"menu":"pizza", "price":10000})   # ['menu', 'price']
+list({"사과", "배"})                     # ['사과', '배']
+nums = list(range(3))                   # [0, 1, 2]
