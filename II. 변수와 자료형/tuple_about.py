8a4add3192ac4555ce785fdcb35b076b7b8f28df
jiyujung <s2019w39@e-mirim.hs.kr>
2021-01-19 23:37:43 +0900

    튜플 활용

--- II. 변수와 자료형/tuple_about.py
+++ II. 변수와 자료형/tuple_about.py
@@ -15,3 +15,19 @@ print(blue)     # 216
 x, y = 1920, 1080
 print(x)        # 1920
 print(y)        # 1080
+
+# 튜플 활용
+x = 2560
+y = 1440
+x, y = y, x      # 직관적인 교환(swap)이 가능
+print(x)        # 1440
+print(y)        # 2560
+a = (123, "abc", True, 123)
+print(a[1])     # 인덱싱 / abc
+print(a[2:])    # 슬라이싱 / (True, 123)
+print(a[:2])    # (123, 'abc')
+# a[1] = 2      # 튜플은 값을 수정할 수 없음
+# index(값) : 값에 해당하는 인덱스를 가져옴
+print(a.index("abc"))   # 1
+# count(값) : 값에 해당하는 요소의 개수를 가져옴
+print(a.count(123))     # 2
