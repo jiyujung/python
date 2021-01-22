af2972fb1dc7ef47c62a101d8e892883ff1260aa
jiyujung <s2019w39@e-mirim.hs.kr>
2021-01-22 17:10:53 +0900

    딕셔너리에 요소 수정

--- II. 변수와 자료형/dictionary_about.py
+++ II. 변수와 자료형/dictionary_about.py
@@ -6,3 +6,7 @@ urls = {"google" : "google.com", "18" : "unesco.org"}
 # 딕셔너리에 요소 추가
 urls["x"] = 2560        # 키 'x'가 딕셔너리에 없으면, 추가
 print(urls)
+
+# 딕셔너리에 요소 수정
+urls["x"] = 1920        # 키 'x'가 딕셔너리에 있으면, 수정
+print(urls)
