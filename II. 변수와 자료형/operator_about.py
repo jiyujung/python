71ddd2241a5b64fe0f77aacec246319a91da9439
jiyujung <s2019w39@e-mirim.hs.kr>
2021-01-31 15:19:09 +0900

    논리 연산자

---
+++ II. 변수와 자료형/operator_about.py
@@ -0,0 +1,11 @@
+# 논리 연산자
+print(not True)         # False
+print(not False)        # True
+print(False and False)  # False
+print(False and True)   # False
+print(True and False)   # False
+print(True and True)    # True
+print(False or False)   # False
+print(False or True)    # True
+print(True or False)    # True
+print(True or True)     # True
