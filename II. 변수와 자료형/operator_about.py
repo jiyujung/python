374c030b68b6beaf092faa10f8438391d27a5b82
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-01 11:17:19 +0900

    비트 연산자

--- II. 변수와 자료형/operator_about.py
+++ II. 변수와 자료형/operator_about.py
@@ -9,3 +9,14 @@ print(False or False)   # False
 print(False or True)    # True
 print(True or False)    # True
 print(True or True)     # True
+
+# 비트 연산자
+print(bin(0b1010))              # 0b1010
+print(bin(0b1010 & 0b0110))     # 0b10
+print(bin(0b1010 | 0b0110))     # 0b1110
+print(bin(0b1010 ^ 0b0110))     # 0b1100
+print(~0b1010)                  # -11
+print(bin(0b1010 << 2))         # 0b101000
+print(0b1010 << 2)              # 40
+print(bin(0b1010 >> 2))         # 0b10
+print(0b1010 >> 2)              # 2
