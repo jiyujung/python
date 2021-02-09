e3f5d67fa4f3f090a275e88fa0ffc705ef18875b
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-09 13:39:48 +0900

    기본적인 while 문

---
+++ III. 프로그램 구성/while_about.py
@@ -0,0 +1,25 @@
+# 기본적인 while 문
+# 형식
+# while 조건식:       # 조건식을 만족하면, 코드 블록을 반복
+#   코드 블록         # 무한 박복이 되지 않게 주의
+
+# x = 3
+# while x < 6:
+#     print(x)      # 3을 무한 반복
+
+x = 3
+while x < 6:
+    print(x)
+    x += 1          # x를 1씩 증가
+
+echo = input()
+while echo != "exit":       # 변수가 exit가 아니면 코드 블록을 실행
+    print(echo)
+    echo = input()          # 새로 입력을 받아 변수를 바꿈
+
+echo = input()
+while True:                 # 코드 블록을 무한 반복
+    if echo == "exit":
+        break               # 조건문을 만족하면 반복문을 멈춤
+    print(echo)
+    echo = input()          # 새로 입력을 받아 변수를 바꿈
