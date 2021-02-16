e0728ebe135b5e0862fbd923afc28e135a9c471c
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-16 11:28:42 +0900

    주사위 굴리는 프로그램 만들기

---
+++ IV. 함수/no_parameter_function.py
@@ -0,0 +1,16 @@
+# 주사위 굴리는 프로그램 만들기
+import random       # 임의의 값을 얻기 위해 사용하는 random 모듈을 가져옴
+# 처음 시작
+n = random.randint(1, 6)        # 1에서 6까지의 정수 중에 하나를 뽑음
+print("결과 : ", n)
+n = random.randint(1, 6)
+print("결과 : ", n)
+n = random.randint(1, 6)
+print("결과 : ", n)
+# 코드 수정
+n = random.randint(1, 6)
+print("6면 주사위 굴린 결과 : ", n)
+n = random.randint(1, 6)
+print("6면 주사위 굴린 결과 : ", n)
+n = random.randint(1, 6)
+print("6면 주사위 굴린 결과 : ", n)
