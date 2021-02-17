f9155f597832d73913989ec9985b1e357d3b20f7
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-17 20:20:41 +0900

    매개 변수가 없는 함수 정의하기

--- IV. 함수/no_parameter_function.py
+++ IV. 함수/no_parameter_function.py
@@ -14,3 +14,11 @@ n = random.randint(1, 6)
 print("6면 주사위 굴린 결과 : ", n)
 n = random.randint(1, 6)
 print("6면 주사위 굴린 결과 : ", n)
+
+# 매개 변수가 없는 함수 정의하기
+# 함수 사용 -- 의미 파악이 쉬워지고 수정이 쉬움
+def rolling_dice():
+    n = random.randint(1, 6)
+    print("6면 주사위 굴린 결과 : ", n)
+rolling_dice()
+rolling_dice()
