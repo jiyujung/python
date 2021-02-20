4f79333eb60b979daa34508c89f36019cbf5d81e
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-20 20:38:24 +0900

    매개 변수에 가변 인수가 있는 함수

--- IV. 함수/parameter_function.py
+++ IV. 함수/parameter_function.py
@@ -11,3 +11,37 @@ rolling_dice(8)
 rolling_dice(10)
 rolling_dice(12)
 rolling_dice(20)
+
+# 매개 변수가 여러 개인 함수
+# 인자 값 - 주사위 여러 번 굴리게 함
+def rolling_dice(pip, repeat):
+    for r in range(1, repeat + 1):
+        n = random.randint(1, pip)
+        print(pip, "면 주사위 굴린 결과", r, " : ", n)
+rolling_dice(6, 1)
+rolling_dice(6, 2)
+rolling_dice(12, 0)
+rolling_dice(20, -3)
+
+# 매개 변수에 가변 인수가 있는 함수
+print("♡")
+print("♡", "♪")
+print("♡", "♪", "♣")
+print("♡", "♪", "♣", "♠")
+print("♡", "♪", "♣", "♠", "★")
+def p(*args):
+    str = ""
+    for a in args:
+        str = str + a
+    print(str)
+p("♡")
+p("♡", "♪")
+p("♡", "♪", "♣", "♠")
+def p(space, space_num, *args):
+    str = args[0]
+    for i in range(1, len(args)):
+        str = str + (space * space_num) + args[i]
+    print(str)
+p(",", 3, "♡", "♪")
+p("☆", 2, "♡", "♪", "♣")
+p("_", 3, "♡", "♪", "♣", "♠")
