82a75494f8acdbb823a6472318593b9329687753
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-24 20:45:44 +0900

    기본 인자 값이 가변 객체인 함수

--- IV. 함수/default_function.py
+++ IV. 함수/default_function.py
@@ -12,3 +12,11 @@ def hello(name, msg="안녕하세요"):
     print(name + "님, " + msg + "!")
 hello("김철수", "오랫만이에요")
 hello("이영희")
+
+# 기본 인자 값이 가변 객체인 함수
+def fn(a, b=[]):
+    b.append(a)
+    print(b)
+fn(3)
+fn(5)
+fn(10)
