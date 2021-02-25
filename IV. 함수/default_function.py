a78379d4b721b40067bf8d1a078e25044e5b7c7e
jiyujung <s2019w39@e-mirim.hs.kr>
2021-02-25 23:47:10 +0900

    디폴트 값을 갖는 함수의 호출

--- IV. 함수/default_function.py
+++ IV. 함수/default_function.py
@@ -20,3 +20,12 @@ def fn(a, b=[]):
 fn(3)
 fn(5)
 fn(10)
+
+# 디폴트 값을 갖는 함수의 호출
+def hello(name, msg="안녕하세요", feeling="♡"):
+    print(name + "님, " + msg + feeling)
+hello("아무개")
+hello("김철수", "오랫만이에요", "♬")
+hello("이영희", "또 만났네요")
+hello("박순이", feeling="!!!")
+hello("MS Jang", msg="Hello~", feeling="♪")
